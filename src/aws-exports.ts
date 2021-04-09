d5dacbc004aab29749cb852490d745bda9fdd277
Anderson Contreira <anderson.contreira@gmail.com>
2021-04-08 22:20:44 -0300

    remocao de arquivos

--- src/aws-exports.ts
+++
@@ -1,8 +0,0 @@
-const awsmobile = {
-  aws_project_profile: 'sigo-lambdas',
-  aws_project_region: 'sa-east-1',
-  aws_project_bucket: 'tcc-td-sigo-production',
-  aws_project_bucket_mock_key: 'indtexbr/qualidade-e-seguranca.db.json'
-};
-
-export default awsmobile;
